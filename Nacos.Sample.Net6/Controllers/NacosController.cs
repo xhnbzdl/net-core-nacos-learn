8042fcbf090a9deb2e3b79837c8f8bb858dc6c3a
hyx <yuxiang.huang@gct-china.com>
2022-04-06 17:03:09 +0800

    添加项目文件。

---
+++ Nacos.Sample.Net6/Controllers/NacosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Nacos.V2;
+
+namespace Nacos.Sample.Net6.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class NacosController : ControllerBase
+    {
+        private readonly INacosConfigService _svc;
+
+        public NacosController(INacosConfigService svc)
+        {
+            _svc = svc;
+        }
+        [HttpGet]
+        public async Task<string> GetSectionAsync(string dataId)
+        {
+            var res = await _svc.GetConfig(dataId, "nacos_demo", 3000);
+            return res;
+        }
+        [HttpGet]
+        public async Task<string> GetDBConnectionString()
+        {
+            return await _svc.GetConfig("DataBase_ConnectionString", "nacos_demo", 3000);
+        }
+    }
+}
---
+++ Nacos.Sample.Net6/Program.cs
@@ -0,0 +1,47 @@
+
+using Nacos.V2.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+#region 注册服务到容器 begin
+// nacos服务注册，发现配置
+//builder.Services.AddNacosAspNet(configuration, "NacosConfig");
+// nacos配置中心，方式一：
+builder.Services.AddNacosV2Config(builder.Configuration, sectionName: "NacosConfig");
+// nocas配置中心，方式二：
+//builder.Services.AddNacosV2Config(x =>
+//{
+//    x.ServerAddresses = new List<string> { "http://yxchatapi.xhnbzdl.cn:8848/" };
+//    x.EndPoint = "";
+//    x.Namespace = "public";
+//    x.UserName = "nacos";
+//    x.Password = "nacos";
+
+//    // this sample will add the filter to encrypt the config with AES.
+//    x.ConfigFilterAssemblies = new List<string> { "BaseApi" };
+
+//    // http协议设置false，rpc设置true
+//    x.ConfigUseRpc = false;
+//});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion 注册服务到容器 end
+
+var app = builder.Build();
+
+#region 配置http请求的管道，中间件 begin
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+#endregion 配置http请求的管道，中间件 end
